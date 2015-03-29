a88f4aae45d43bf5cb0a5b9361d01874d24c3dbd
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-29 09:02:06 -0400

    Make inputs and formulas different shapes. Fixes #87.

--- analysis/cellplot.r
+++ analysis/cellplot.r
@@ -11,9 +11,9 @@ cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2"
 xbreaks <- c(1,10,100,1000,10000,100000)
 ybreaks <- c(1,10,100,1000,10000,100000)
 
-p2 <- ggplot(file.read, aes(x = N, y = COUNT, color = CATEGORY)) + scale_colour_manual(name="Cell Type",values=cbbPalette,labels=c("Formula", "Input")) +
-  geom_point(size=3, alpha=0.4) + scale_x_log10(breaks=xbreaks,labels=comma) + scale_y_log10(breaks=ybreaks,labels=comma) + geom_smooth(level = 0.99) +
-  xlab("Cell Count") + ylab("Count of Spreadsheets") + theme(legend.position="bottom")
+p2 <- ggplot(file.read, aes(x = N, y = COUNT, color = CATEGORY)) + scale_colour_manual(name="Cell Type",values=cbbPalette,labels=c("Formula", "Input")) + scale_shape_manual(name="Cell Type", values=c(16,17),labels=c("Formula", "Input")) + 
+  geom_point(size=3, alpha=0.4, aes(shape = CATEGORY)) + scale_x_log10(breaks=xbreaks,labels=comma) + scale_y_log10(breaks=ybreaks,labels=comma) + geom_smooth(level = 0.99) +
+  xlab("Cell Count") + ylab("Spreadsheet Count") + theme(legend.position="bottom")
 
 # + theme(legend.position=c(.9, .8))
 
@@ -21,4 +21,4 @@ p2 <- ggplot(file.read, aes(x = N, y = COUNT, color = CATEGORY)) + scale_colour_
 #  scale_y_log10() + xlab("Count of Non-Empty Cells") + ylab("Count of Spreadsheets")
 
 
-plot(p2)
+# plot(p2)
