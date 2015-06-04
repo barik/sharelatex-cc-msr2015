8ce319142135d5ee7af876d040a0c402c997f0db
Kevin Lubick <kjlubick@ncsu.edu>
2015-06-03 17:57:03 -0700

    Add basic mindmap

---
+++ ExtendedJournal/msr-journal.mm
@@ -0,0 +1,28 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1432102162290" ID="ID_1054660145" MODIFIED="1432102162290" TEXT="Fuse">
+<node CREATED="1433380227708" ID="ID_1695623678" MODIFIED="1433380241833" POSITION="right" TEXT="Things we cut and should put back in">
+<node CREATED="1433380263401" ID="ID_56241149" MODIFIED="1433380317508" TEXT="Pretty json of data (fix mint package and document fix)"/>
+<node CREATED="1433380318245" FOLDED="true" ID="ID_590567005" MODIFIED="1433381155728" TEXT="Talking about spreadsheets that went across domains">
+<node CREATED="1433381084170" ID="ID_1929758212" MODIFIED="1433381111851" TEXT="Only propagated 589 times, and that the highest number of spreadsheets is only presented across 3 domains."/>
+</node>
+<node CREATED="1433381156547" ID="ID_23366653" MODIFIED="1433381180993" TEXT="Top domain table and suffixes table"/>
+<node CREATED="1433381181745" ID="ID_1069585022" MODIFIED="1433381205693" TEXT="Comparison of function counts across corpora"/>
+<node CREATED="1433381167232" ID="ID_1880677646" MODIFIED="1433381228766" TEXT="Classification of Euses v Fuse"/>
+<node CREATED="1433381229231" ID="ID_1041241807" MODIFIED="1433381285503" TEXT="Performing example queries on the data using Mongodb"/>
+<node CREATED="1433381286567" ID="ID_1092132106" MODIFIED="1433381371670" TEXT="The three spreadsheets from euses that are in fuse (?)"/>
+<node CREATED="1433381381450" ID="ID_1462026570" MODIFIED="1433381391149" TEXT="Break down by media type"/>
+<node CREATED="1433381443553" ID="ID_1849122404" MODIFIED="1433381452470" TEXT="Argument about programming in spreadsheets"/>
+</node>
+<node CREATED="1433380243965" ID="ID_503043497" MODIFIED="1433380252518" POSITION="right" TEXT="Things we didn&apos;t have in the first time and should">
+<node CREATED="1433380884659" ID="ID_93183054" MODIFIED="1433380930576" TEXT="Talk about the software we released and roughly how to use it"/>
+<node CREATED="1433380931931" ID="ID_394769257" MODIFIED="1433380967332" TEXT="A comparison of Euses, Fuse and Enron (where possible, using Felienne&apos;s table)"/>
+<node CREATED="1433381305601" ID="ID_463634144" MODIFIED="1433381323143" TEXT="Add more about what our analysis metadata consists of"/>
+<node CREATED="1433381391847" ID="ID_1485637343" MODIFIED="1433381405282" TEXT="Tika types we omitted and cost to re-evaluate"/>
+</node>
+<node CREATED="1433380253345" ID="ID_1296747801" MODIFIED="1433380790374" POSITION="right" TEXT="Other things we should change">
+<node CREATED="1433380790374" ID="ID_715664423" MODIFIED="1433380794527" TEXT="Make figures bigger"/>
+</node>
+<node CREATED="1433380258290" ID="ID_1944862310" MODIFIED="1433380394253" POSITION="right" TEXT="Reviewer&apos;s comments we could not address due to space"/>
+</node>
+</map>
