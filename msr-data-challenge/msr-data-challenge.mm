7e9f9ab6454babe64b28b29e10ef535ada0033cd
Kevin Lubick <kjlubick@ncsu.edu>
2015-06-08 07:09:39 -0700

    Add Mindmap
    Brainstorming ideas and finding related work

---
+++ msr-data-challenge/msr-data-challenge.mm
@@ -0,0 +1,82 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1433769803145" ID="ID_957739977" MODIFIED="1433769910489" STYLE="fork" TEXT="data challenge">
+<node CREATED="1433769900751" ID="ID_941494571" MODIFIED="1433770384199" POSITION="right" TEXT="Motivational points">
+<node CREATED="1433772444953" ID="ID_1203238999" MODIFIED="1433772455009" TEXT="Similar to MSR paper"/>
+</node>
+<node CREATED="1433770384559" ID="ID_1859037892" MODIFIED="1433770398852" POSITION="right" TEXT="Research used similar dataset up to this point">
+<node CREATED="1433770606440" ID="ID_1040822107" MODIFIED="1433770616563" TEXT="Inferring templates from spreadsheets">
+<node CREATED="1433770616566" ID="ID_1613919503" MODIFIED="1433770618846" TEXT="http://dl.acm.org/citation.cfm?id=1134312"/>
+<node CREATED="1433770619526" ID="ID_527340471" MODIFIED="1433770627999" TEXT="Abraham 2006"/>
+</node>
+<node CREATED="1433770631263" ID="ID_996155891" MODIFIED="1433770650452" TEXT="GoalDebug: A spreadsheet debugger for end users">
+<node CREATED="1433770657317" ID="ID_623669730" MODIFIED="1433770658441" TEXT="http://dl.acm.org/citation.cfm?id=1248858"/>
+<node CREATED="1433770658937" ID="ID_1485684037" MODIFIED="1433770666205" TEXT="Abraham 2007"/>
+</node>
+<node CREATED="1433770699365" ID="ID_619452853" MODIFIED="1433770761300" TEXT="Detecting and visualizing inter-worksheet smells in spreadsheets">
+<node CREATED="1433770714609" ID="ID_176156457" MODIFIED="1433770716914" TEXT="http://dl.acm.org/citation.cfm?id=2337275"/>
+<node CREATED="1433770718676" ID="ID_740984515" MODIFIED="1433770727059" TEXT="Hermans 2012"/>
+</node>
+<node CREATED="1433770762298" ID="ID_164533200" MODIFIED="1433770822317" TEXT="Detecting code smells in spreadsheet formulas">
+<node CREATED="1433770775811" ID="ID_1550162004" MODIFIED="1433770784731" TEXT="http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=6405300"/>
+<node CREATED="1433770785388" ID="ID_403728778" MODIFIED="1433770790401" TEXT="Hermans 2012"/>
+</node>
+<node CREATED="1433770823612" ID="ID_163486514" MODIFIED="1433770849362" TEXT="On the empirical evaluation of fault localization techniques for spreadsheets">
+<node CREATED="1433770849366" ID="ID_140962916" MODIFIED="1433770851264" TEXT="http://link.springer.com/chapter/10.1007/978-3-642-37057-1_6"/>
+<node CREATED="1433770852294" ID="ID_1556335521" MODIFIED="1433770857808" TEXT="Hofer 2013"/>
+</node>
+<node CREATED="1433770882642" ID="ID_1647087342" MODIFIED="1433770982206" TEXT="Data clone detection and visualization in spreadsheets">
+<node CREATED="1433770897623" ID="ID_108928115" MODIFIED="1433770898493" TEXT="http://dl.acm.org/citation.cfm?id=2486827"/>
+<node CREATED="1433770899068" ID="ID_1963479587" MODIFIED="1433770902454" TEXT="Hermans 2013"/>
+</node>
+<node CREATED="1433770982826" ID="ID_325252452" MODIFIED="1433770997151" TEXT="FlashExtract: a framework for data extraction by examples">
+<node CREATED="1433770998596" ID="ID_669887764" MODIFIED="1433770999554" TEXT="http://dl.acm.org/citation.cfm?id=2594333"/>
+<node CREATED="1433771000599" ID="ID_1259288932" MODIFIED="1433771012612" TEXT="Le 2014"/>
+</node>
+<node CREATED="1433771020292" ID="ID_1652236257" MODIFIED="1433771056774" TEXT="SheetDiff: a tool for identifying changes in spreadsheets">
+<node CREATED="1433771058427" ID="ID_217053597" MODIFIED="1433771059772" TEXT="http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=5635197"/>
+<node CREATED="1433771060318" ID="ID_458377177" MODIFIED="1433771067558" TEXT="Chambers 2010"/>
+</node>
+<node CREATED="1433771070200" ID="ID_962536011" MODIFIED="1433771100536" TEXT="Avoiding, finding and fixing spreadsheet errors - a survey of automated spreadsheet QA">
+<node CREATED="1433771243593" ID="ID_1268701325" MODIFIED="1433771245352" TEXT="http://www.sciencedirect.com/science/article/pii/S0164121214000788"/>
+<node CREATED="1433771100539" ID="ID_78588700" MODIFIED="1433771108386" TEXT="Jannach 2014"/>
+</node>
+<node CREATED="1433771111896" ID="ID_173547328" MODIFIED="1433771197882" TEXT="On the empirical evaluation of similarity coefficients for spreadsheets fault localization">
+<node CREATED="1433771197886" ID="ID_1540072742" MODIFIED="1433771220827" TEXT="http://link.springer.com/article/10.1007/s10515-014-0145-3"/>
+<node CREATED="1433771221603" ID="ID_1112498662" MODIFIED="1433771230617" TEXT="Hofer 2014"/>
+</node>
+<node CREATED="1433771284865" ID="ID_1652735465" MODIFIED="1433771323306" TEXT="A quality model for spreadsheets">
+<node CREATED="1433771323310" ID="ID_1147402582" MODIFIED="1433771325003" TEXT="http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=6511816"/>
+<node CREATED="1433771325276" ID="ID_1014367940" MODIFIED="1433771330853" TEXT="Cunha 2012"/>
+</node>
+</node>
+<node CREATED="1433771632251" ID="ID_352311751" MODIFIED="1433771758884" POSITION="right" TEXT="Stuff to do with Fuse">
+<node CREATED="1433771735477" ID="ID_1268053400" MODIFIED="1433771794577" TEXT="Small % of spreadsheets use formulas.  Can you detect spreadsheets that could use formulas but don&apos;t?"/>
+<node CREATED="1433771795192" ID="ID_692702496" MODIFIED="1433771985679" TEXT="How does the complexity of consumer formulas stack up to enron formulas?"/>
+<node CREATED="1433771990169" ID="ID_857998421" MODIFIED="1433772014649" TEXT="How do spreadsheets and their formulas change over time?"/>
+<node CREATED="1433772015098" ID="ID_467712340" MODIFIED="1433772087558" TEXT="Are there formulas currently in use that could be simplified by the addition of other functions?  e.g. if to switch"/>
+<node CREATED="1433772088492" ID="ID_1241447025" MODIFIED="1433772219669" TEXT="What are the precision/recall of various static analyses"/>
+<node CREATED="1433772220243" ID="ID_373761824" MODIFIED="1433772282755" TEXT="Is there any sensitive data inadvertantly leaked onto the public web?  If so, can there be a user intervention to minimize this (recommend encryption, etc)"/>
+<node CREATED="1433772284014" ID="ID_1243999910" MODIFIED="1433772375285" TEXT="Apache POI chokes on some of the input, but is the best tool we found to analyze at scale.  Are there better alternatives? (faster, fewer &quot;corrupted files&quot;, less ram use)"/>
+<node CREATED="1433772375987" ID="ID_1329093280" MODIFIED="1433772430103" TEXT="How likely are formulas to co-exist across sheets?  can we do collaboritive filtering or something to recommend them?"/>
+</node>
+<node CREATED="1433770399436" ID="ID_1105293409" MODIFIED="1433770463594" POSITION="right" TEXT="Apache POI overview">
+<node CREATED="1433770467673" ID="ID_1685002324" MODIFIED="1433770471857" TEXT="R1C1 notation"/>
+<node CREATED="1433770473139" ID="ID_369969813" MODIFIED="1433770503688" TEXT="Ram consumption"/>
+<node CREATED="1433770504247" ID="ID_328220008" MODIFIED="1433770541686" TEXT="Look at test cases for counts/verification"/>
+</node>
+<node CREATED="1433769919184" ID="ID_1806368923" MODIFIED="1433769947676" POSITION="left" TEXT="title"/>
+<node CREATED="1433769949078" ID="ID_355983386" MODIFIED="1433769955900" POSITION="left" TEXT="background on where the data came from"/>
+<node CREATED="1433769956922" ID="ID_1179044188" MODIFIED="1433769963628" POSITION="left" TEXT="Where the data set currently is"/>
+<node CREATED="1433769963854" ID="ID_954567199" MODIFIED="1433769972074" POSITION="left" TEXT="What types of data the dataset contains"/>
+<node CREATED="1433769972871" ID="ID_1762955338" MODIFIED="1433769997782" POSITION="left" TEXT="How the data can be obtained"/>
+<node CREATED="1433769998510" ID="ID_561040413" MODIFIED="1433770004639" POSITION="left" TEXT="How large the dataest is"/>
+<node CREATED="1433770006368" ID="ID_1747892744" MODIFIED="1433770014485" POSITION="left" TEXT="Any challenges in obtaining the data (?)"/>
+<node CREATED="1433770015230" ID="ID_1312238500" MODIFIED="1433770234421" POSITION="left" TEXT="Description of how to make the data set easily accessible (?)"/>
+<node CREATED="1433770235570" ID="ID_564668674" MODIFIED="1433770244998" POSITION="left" TEXT="Motivation for why the dataset is appropriate"/>
+<node CREATED="1433770245670" ID="ID_1182697265" MODIFIED="1433770253011" POSITION="left" TEXT="A sample list of ways the data could be used"/>
+<node CREATED="1433770253270" ID="ID_635775684" MODIFIED="1433770268264" POSITION="left" TEXT="Any research that has used the dataset or a similar dataset up to this point"/>
+<node CREATED="1433770268497" ID="ID_1954756080" MODIFIED="1433770279232" POSITION="left" TEXT="Any other details the authors feel is appropriate"/>
+<node CREATED="1433770279585" ID="ID_1965016259" MODIFIED="1433770286850" POSITION="left" TEXT="Short bio of the authors"/>
+</node>
+</map>
